d652769bae4a5561a947070a1848f57657c5c47c
JeremySun0731 <edwardsun200407@gmail.com>
2025-11-21 15:25:27 -0500

    finish

--- src/Polynomial1L.java
+++ src/Polynomial1L.java
@@ -1,4 +1,5 @@
-import java.util.Map;
+import components.map.Map;
+import components.map.Map1L;
 
 /**
  * Kernel implementation for the Polynomial component using a Map-based
@@ -12,8 +13,10 @@ import java.util.Map;
  * @correspondence this = Σ (rep.get(d) * x^d) for all degrees d in rep.
  */
 public class Polynomial1L extends PolynomialSecondary {
-
-    private Map<Integer, Integer> rep;
+    /**
+     * Representation of the polynomial.
+     */
+    private Map<Integer, Double> rep;
 
     /**
      * Creates a new empty polynomial.
@@ -22,43 +25,63 @@ public class Polynomial1L extends PolynomialSecondary {
      */
     public Polynomial1L() {
         // initialize representation
+        this.rep = new Map1L<>();
     }
 
     @Override
-    public void clear() {
-        // TODO Auto-generated method stub
+    public final void clear() {
+        this.rep.clear();
     }
 
     @Override
-    public void newPolynomial() {
-        // TODO Auto-generated method stub
+    public final void newPolynomial() {
+        this.rep = new Map1L<>();
     }
 
     @Override
-    public void setCoefficient(int degree, double value) {
-        // TODO Auto-generated method stub
+    public final void setCoefficient(int degree, double value) {
+        if (value == 0.0) {
+            this.rep.remove(degree);
+        } else if (this.rep.hasKey(degree)) {
+            this.rep.replaceValue(degree, value);
+        } else {
+            this.rep.add(degree, value);
+        }
     }
 
     @Override
-    public double getCoefficient(int degree) {
-        // TODO Auto-generated method stub
-        return 0;
+    public final double getCoefficient(int degree) {
+        double result = 0.0;
+        if (this.rep.hasKey(degree)) {
+            result = this.rep.value(degree);
+        }
+        return result;
     }
 
     @Override
-    public int getDegree() {
-        // TODO Auto-generated method stub
-        return 0;
+    public final int getDegree() {
+        int maxDegree = -1;
+        for (Map.Pair<Integer, Double> pair : this.rep) {
+            int degree = pair.key();
+            if (degree > maxDegree) {
+                maxDegree = degree;
+            }
+        }
+        return maxDegree;
     }
 
     @Override
-    public Polynomial newInstance() {
-        // TODO Auto-generated method stub
-        return null;
+    public final Polynomial newInstance() {
+        return new Polynomial1L();
     }
 
     @Override
-    public void transferFrom(Polynomial source) {
-        // TODO Auto-generated method stub
+    public final void transferFrom(Polynomial source) {
+        assert source instanceof Polynomial1L : "Violation of: source is of dynamic type Polynomial1L";
+
+        Polynomial1L local = (Polynomial1L) source;
+
+        // Correct OSU-component transfer of the representation
+        this.rep.transferFrom(local.rep);
     }
 }
